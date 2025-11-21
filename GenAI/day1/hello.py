d2b72686f216a92bbb12142274253a3e555c15ed
Ahamedjasir <jasir4904@gmail.com>
2025-11-21 12:48:08 +0530

    operators

--- GenAI/day1/hello.py
+++ GenAI/day1/hello.py
@@ -1,7 +1,20 @@
-name="jasir"
-age=22
-location="thittuvilai"
-favSports="football"
-height=6.02
-weight=83
-print("my name is:",name ,"my age is:",age ,"iam coming from:",location ,"my fav sports:",favSports ,"my height:",height ,"my weight:",weight)
+name = "jasir"
+age = 22
+location = "thittuvilai"
+favSports = "football"
+height = 6.02
+weight = 83
+print(
+    "my name is:",
+    name,
+    "my age is:",
+    age,
+    "iam coming from:",
+    location,
+    "my fav sports:",
+    favSports,
+    "my height:",
+    height,
+    "my weight:",
+    weight,
+)
---
+++ GenAI/day1/operator.py
@@ -0,0 +1,26 @@
+a = 15
+b = 20
+a /= b
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+mod = a % b
+com1 = a == b
+com2 = a != b
+grt1 = a > b
+grt2 = a < b
+grt3 = a <= b
+grt4 = a >= b
+print("value of add is", add)
+print("value of sub is", sub)
+print("value of mul is", mul)
+print("value of div is", div)
+print("value of mod is", mod)
+print(com1)
+print(com2)
+print(grt1)
+print(grt2)
+print(grt3)
+print(grt4)
+print(a)
