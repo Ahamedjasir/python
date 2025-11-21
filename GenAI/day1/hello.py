e9a69f822175ae80136a4b0237b5dfc2ff034d24
Ahamedjasir <jasir4904@gmail.com>
2025-11-21 11:55:31 +0530

    day1

---
+++ GenAI/day1/hello.py
@@ -0,0 +1,7 @@
+name="jasir"
+age=22
+location="thittuvilai"
+favSports="football"
+height=6.02
+weight=83
+print("my name is:",name ,"my age is:",age ,"iam coming from:",location ,"my fav sports:",favSports ,"my height:",height ,"my weight:",weight)
--- big.py
+++
@@ -1,8 +0,0 @@
-print("enter the first number  :")
-x = int(input())
-print("enter the second number :")
-y= int(input())
-if x>y:
-    print("x is greater:",x)
-else:
-    print("y is greater:",y)
